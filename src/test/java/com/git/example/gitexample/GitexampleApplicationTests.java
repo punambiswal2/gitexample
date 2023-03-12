0b3e3daac788994d3eb76ef6bbb1805c75d16adb
punambiswal <punambiswal2@gmail.com>
2023-03-12 19:54:50 -0400

    This is my first git example project

---
+++ src/test/java/com/git/example/gitexample/GitexampleApplicationTests.java
@@ -0,0 +1,15 @@
+package com.git.example.gitexample;
+
+import org.junit.Assert;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitexampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+		//Assert.assertEquals("hello","hola");
+	}
+
+}
